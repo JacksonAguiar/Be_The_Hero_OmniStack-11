bab2783d6e76b2be4b0d3e3f461761415eb9f569
jackson wellington <jacksonwsa377@gmail.com>
2020-03-28 10:01:03 -0300

    aplicando testes automatizados jest e supertest

---
+++ backend/tests/unit/generateUniqueId.spec.js
@@ -0,0 +1,7 @@
+const generateUniqueId = require('../../src/utils/generetedUniqueId');
+
+describe('Generate Unique ID', ()=>{
+    it('should generate an unique ID', ()=>{
+        expect(id).toHaveLength(8);
+    })
+})
